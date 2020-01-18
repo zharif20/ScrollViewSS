8a7a59c0354b2d9ca13afe8d40c95a027d50829a
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2020-01-19 02:24:25 +0800

    Initial Commit

---
+++ ScrollViewSS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewSS
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 19/01/2020.
+//  Copyright © 2020 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
